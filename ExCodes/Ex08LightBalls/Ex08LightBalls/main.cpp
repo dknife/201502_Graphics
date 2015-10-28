59a8b4307d6f08017b85179ee8481376a419b012
Dknife <young.min.kang@gmail.com>
2015-10-28 10:22:30 +0900

    light test

---
+++ ExCodes/Ex08LightBalls/Ex08LightBalls/main.cpp
@@ -0,0 +1,156 @@
+#ifdef WIN32
+#include <Windows.h>
+#include <gl/GL.h>
+#include <gl/glut.h>
+#else // mac
+#include <OpenGL/OpenGL.h>
+#include <GLUT/GLUT.h>
+#endif
+
+#include <stdio.h>
+#include <math.h>
+
+
+float aspRatio = 1.0;
+
+
+GLfloat matSpec[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat matDiff[] = { 1.0, 1.0, 0.0, 1.0 };
+GLfloat matAmbi[] = { 0.5, 0.1, 0.1, 1.0 };
+GLfloat matShin[] = { 127.0 };
+
+GLfloat light[] = { 1.0, 1.0, 1.0, 1.0 };
+GLfloat lightPos[] = { -1.0, 1.0, 1.0, 1.0 };
+
+void LightSet(void) {
+	glMaterialfv(GL_FRONT, GL_SPECULAR, matSpec);
+	glMaterialfv(GL_FRONT, GL_DIFFUSE, matDiff);
+	glMaterialfv(GL_FRONT, GL_AMBIENT, matAmbi);
+	glMaterialfv(GL_FRONT, GL_SHININESS, matShin);
+
+	glLightfv(GL_LIGHT0, GL_SPECULAR, light);
+	glLightfv(GL_LIGHT0, GL_DIFFUSE, light);
+	glLightfv(GL_LIGHT0, GL_AMBIENT, light);
+
+	glEnable(GL_LIGHTING);
+	glEnable(GL_LIGHT0);
+}
+
+void LightPosition(void) {
+	// change light position
+	float r = 5;
+	static float angle = 0.0;
+	lightPos[0] = r * cos(angle);
+	lightPos[1] = 0.0;
+	lightPos[2] = r * sin(angle);
+	angle += 0.05;
+	glLightfv(GL_LIGHT0, GL_POSITION, lightPos);
+}
+
+void drawAxes() {
+	glBegin(GL_LINES);
+	glColor3f(1, 0, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(1, 0, 0); // x
+	glColor3f(0, 1, 0);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 1, 0); // y
+	glColor3f(0, 0, 1);
+	glVertex3f(0, 0, 0);
+	glVertex3f(0, 0, 1); // z
+	glEnd();
+}
+
+
+
+void drawSpheres(void) {
+	for (int x = -5; x <= 5; x++) {
+		for (int y = -5; y <= 5; y++) {
+			glPushMatrix();
+			glTranslatef(x, y, 0);
+			glutSolidSphere(0.5, 30, 30);
+			glPopMatrix();
+		}
+	}
+}
+
+void myDisplay() {
+	glClear(GL_DEPTH_BUFFER_BIT | GL_COLOR_BUFFER_BIT);
+
+	glMatrixMode(GL_MODELVIEW);
+	glLoadIdentity();
+	
+	LightPosition();
+
+	glDisable(GL_LIGHTING);
+	glLineWidth(5);
+	drawAxes();
+
+	// light source
+	glPushMatrix();
+	glTranslatef(lightPos[0], lightPos[1], lightPos[2]);
+	glutSolidSphere(0.1, 10, 10);
+	glPopMatrix();
+
+	glEnable(GL_LIGHTING);
+	drawSpheres();
+
+
+	glutSwapBuffers();
+}
+
+
+
+void setCamera(void) {
+	glMatrixMode(GL_PROJECTION);
+	glLoadIdentity();
+	glOrtho(-8*aspRatio, 8*aspRatio, -8, 8, -10, 10);
+	glutPostRedisplay();
+}
+
+void reshape(int w, int h) {
+	aspRatio = float(w) / h;
+	glViewport(0, 0, w, h);
+
+	setCamera();
+}
+
+void keyboard(unsigned char k, int x, int y) {
+	switch (k) {
+	case 27: exit(0);
+	}
+	setCamera();
+
+}
+
+void GLInit(void) {
+
+	glEnable(GL_DEPTH_TEST);
+	glClearColor(1.0, 1.0, 1.0, 1.0);
+
+	LightSet();
+}
+
+int main(int argc, char **argv) {
+	glutInit(&argc, argv);
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_DEPTH | GLUT_RGBA);
+	glutInitWindowPosition(0, 0);
+	glutInitWindowSize(512, 512);
+	glutCreateWindow("lights");
+
+	GLInit();
+
+	// display callback and idle callback
+	glutDisplayFunc(myDisplay);
+	glutIdleFunc(myDisplay);
+
+	//reshape callback
+	glutReshapeFunc(reshape);
+
+	// keyboard callback
+	glutKeyboardFunc(keyboard);
+
+	glutMainLoop();
+
+	return 0;
+}
